ec894e7d7f3f147ffc73bf4a934fbeb95369c774
Andreas Beyer <abeyer@posteo.de>
2020-05-10 19:03:08 +0200

    [app] minors

--- components/Screen.js
+++ components/Screen.js
@@ -1,24 +1,23 @@
 //@format
 //@flow
 
-import React from 'react';
-import styled from 'styled-native-components';
-import {SafeAreaView} from 'react-native';
+import React from "react";
+import styled from "styled-native-components";
+import { SafeAreaView } from "react-native";
 
 const ScreenWrapper = styled.View`
   height: 100%;
   width: 100%;
   justify-content: center;
   align-items: center;
-  background-color: ${p => p.background ? p.background : '$background' };
+  background-color: ${(p) => (p.background ? p.background : "$background")};
 `;
 
-const Screen = ({children, background}) => {
+const Screen = ({ children, background }) => {
   return (
-    <SafeAreaView>
-      <ScreenWrapper background={background}>
-        {children}
-      </ScreenWrapper>
+    // eslint-disable-next-line
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScreenWrapper background={background}>{children}</ScreenWrapper>
     </SafeAreaView>
   );
 };
