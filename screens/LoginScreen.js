370f3842c2f458b3a52e80227a87aef6300368f4
Andreas Beyer <abeyer@posteo.de>
2020-06-11 21:25:51 +0200

    use api as auth proxy

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -8,24 +8,17 @@ import { Platform } from "react-native";
 import Button from "../components/Button";
 
 import { authorize } from "react-native-app-auth";
-import {
-  SPOTIFY_CLIENT_ID,
-  SPOTIBET_API_DEV,
-  SPOTIBET_API_PROD,
-} from "../consts";
+import { SPOTIFY_CLIENT_ID, SPOTIBET_API_ENDPOINT } from "../consts";
 
 const config = {
   clientId: SPOTIFY_CLIENT_ID,
-  redirectUrl:
-    Platform.OS === "ios"
-      ? "com.spotibet:/oauthredirect"
-      : "com.spotibet://oauthredirect",
+  redirectUrl: `com.spotibet:${
+    Platform.OS === "ios" ? "/" : "//"
+  }oauthredirect`,
   scopes: ["user-read-email", "user-read-private"],
   serviceConfiguration: {
     authorizationEndpoint: "https://accounts.spotify.com/authorize",
-    tokenEndpoint: __DEV__
-      ? `${SPOTIBET_API_DEV}/auth`
-      : `${SPOTIBET_API_PROD}/auth`,
+    tokenEndpoint: `${SPOTIBET_API_ENDPOINT}/auth?os=${Platform.OS}`,
   },
 };
 
