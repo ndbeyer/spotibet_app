b12732dcdce7979c5c2f38ebeb094f6838893663
Andreas Beyer <abeyer@posteo.de>
2020-07-01 14:28:31 +0200

    upgrade Loading

--- components/Loading.js
+++ components/Loading.js
@@ -1,18 +1,21 @@
 import React from "react";
-import styled from "styled-native-components";
-import { Paragraph } from "./Text";
+import { ActivityIndicator } from "react-native";
+import styled, { useTheme } from "styled-native-components";
 
 const Wrapper = styled.View`
-  flex: 1;
+  height: 100%;
   width: 100%;
   justify-content: center;
   align-items: center;
 `;
 
-const Loading = () => (
-  <Wrapper>
-    <Paragraph>Loading...</Paragraph>
-  </Wrapper>
-);
+const Loading = () => {
+  const theme = useTheme();
+  return (
+    <Wrapper>
+      <ActivityIndicator color={theme.colors.neutral0} />
+    </Wrapper>
+  );
+};
 
 export default Loading;
