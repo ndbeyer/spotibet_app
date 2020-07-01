c16ea42099a7b59074f3dfd6b93fad85a0125178
Andreas Beyer <abeyer@posteo.de>
2020-07-01 14:50:36 +0200

    upgrade button

--- components/Button.js
+++ components/Button.js
@@ -1,34 +1,51 @@
 import React from "react";
 import styled from "styled-native-components";
 
+import Loading from "../components/Loading";
+import { Label } from "../components/Text";
+
 const Wrapper = styled.TouchableOpacity`
-  min-width: 100px;
-  height: 30px;
+  min-width: 10rem;
+  background-color: ${(p) => p.color};
+  border-width: ${(p) => (p.outline ? 1 : 0)}px;
+  border-style: solid;
+  border-color: ${(p) => p.color};
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   border-radius: 1rem;
-`;
-
-const Label = styled.Text`
-  color: ${(p) => (p.disabled ? p.colorDisabled : p.color)};
+  margin: ${(p) => p.margin};
 `;
 
 const Button = ({
-  label,
+  label = "I have no label",
+  outline,
   loading,
   disabled,
   onPress,
   textColor = "$accentText0",
-  colorDisabled = "$neutral2",
+  colorDisabled = "$neutral3",
+  color = "$background1",
+  light = true,
+  margin = "1rem",
 }) => {
   return (
-    <Wrapper onPress={onPress} disabled={disabled}>
+    <Wrapper
+      onPress={onPress}
+      disabled={disabled}
+      outline={outline}
+      color={color}
+      margin={margin}
+    >
       <Label
-        color={textColor}
+        light={light}
+        color={disabled ? colorDisabled : textColor}
         colorDisabled={colorDisabled}
         disabled={disabled || loading}
+        margin="1rem"
       >
-        {loading ? "..." : label}
+        {loading ? <Loading /> : null}
+        {label}
       </Label>
     </Wrapper>
   );
