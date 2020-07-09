41cfdf2e8ce78d63683aeb2b006f2ea7d72212a3
Andreas Beyer <abeyer@posteo.de>
2020-07-09 12:48:59 +0200

    debug

--- components/CardWrapper.js
+++ components/CardWrapper.js
@@ -7,7 +7,7 @@ const PotentiallyTouchable = styled((props) =>
 )`
   align-self: stretch;
   margin: 0.5rem 1rem;
-  border-radius: $borderRadius2;
+  border-radius: 1rem;
   background-color: $background0;
   elevation: 1;
   padding: 1rem;
