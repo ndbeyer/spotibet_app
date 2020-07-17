fb9d4fa1215660a0dfe3dc11cfa4de2f7da32c71
Andreas Beyer <abeyer@posteo.de>
2020-07-17 10:19:50 +0200

    upgrade BetStats

--- components/BetStats.js
+++ components/BetStats.js
@@ -68,20 +68,41 @@ const BarWrapper = styled.View`
 `;
 
 const StyledGradient = styled(Gradient).attrs((p) => ({
-  colors: p.currentUserWins
-    ? [p.theme.colors.accent0, p.theme.colors.background0]
-    : [p.theme.colors.neutral4, p.theme.colors.background0],
+  colors:
+    p.currentUserWins && p.presentationType === "REPORT"
+      ? [p.theme.colors.accent0, p.theme.colors.background0]
+      : [p.theme.colors.neutral4, p.theme.colors.background0],
 }))``;
 
-const BetVisualizer = ({
+const BetStats = ({
+  listeners,
   predictedListeners,
-  currentListeners,
+  currentListeners = 0,
+  type,
+  presentationType,
+  endDate,
+  currentUserSupports = true,
   nBarHeightMax = 10,
   nBarWidth = 7,
-  type,
-  endDate,
-  currentUserSupports,
+  startDate, // TODO: do we need this?
+  quote, // TODO: this needs to be visualized
+  currentUserAmount, // TODO: this needs to be visualized
+}: {
+  listeners?: number,
+  predictedListeners?: number,
+  currentListeners?: number,
+  type?: "HIGHER" | "LOWER",
+  presentationType?: "CREATE" | "REPORT",
+  endDate?: string,
+  currentUserSupports?: boolean,
+  nBarHeightMax?: number,
+  nBarWidth?: number,
+  startDate?: string, // TODO: do we need this?
+  quote?: number, // TODO: this needs to be visualized
+  currentUserAmount?: number, // TODO: this needs to be visualized
 }) => {
+  predictedListeners = listeners || predictedListeners;
+
   const predictedIsHigher = React.useMemo(
     () => predictedListeners > currentListeners,
     [currentListeners, predictedListeners]
@@ -104,111 +125,92 @@ const BetVisualizer = ({
   const [showDifference, setShowDifference] = React.useState(false);
 
   return (
-    <BetVisualizerWrapper onPress={() => setShowDifference((b) => !b)}>
-      <GraphSection
-        topPadding={
-          !predictedIsHigher || (predictedIsHigher && !userPredictsOverShoot)
-        }
-      >
-        <Bar
-          height={
-            predictedIsHigher
-              ? nBarHeightMax / 2 + "rem"
-              : nBarHeightMax + "rem"
+    <Wrapper>
+      <BetVisualizerWrapper onPress={() => setShowDifference((b) => !b)}>
+        <GraphSection
+          topPadding={
+            !predictedIsHigher || (predictedIsHigher && !userPredictsOverShoot)
           }
-          width={nBarWidth + "rem"}
         >
-          <StyledGradient currentUserWins={currentUserWins} />
-          <Positioner textAboveBar>
-            <Paragraph>{getNumberWithSuffix(currentListeners)}</Paragraph>
-          </Positioner>
-        </Bar>
-        {showDifference ? (
-          <DifferenceWrapper
-            height={nBarHeightMax + "rem"}
-            width={nBarWidth + "rem"}
-          >
-            <Paragraph size="s" color="$neutral3">
-              {userPredictsOverShoot ? ">" : "<"}
-            </Paragraph>
-            <Paragraph size="s" margin="0 0 0.5rem 0" color="$neutral3">
-              {predictedIsHigher ? "+" : null}
-              {getNumberWithSuffix(predictedListeners - currentListeners)}
-            </Paragraph>
-            <Paragraph size="s" color="$neutral3">
-              {predictedIsHigher ? "+" : null}
-              {(
-                ((predictedListeners - currentListeners) / currentListeners) *
-                100
-              ).toFixed()}
-              %
-            </Paragraph>
-          </DifferenceWrapper>
-        ) : null}
-
-        <BarWrapper>
-          {userPredictsOverShoot ? (
-            <TendencyPositioner
-              height={nBarHeightMax / 2 + "rem"}
-              width={nBarWidth + "rem"}
-            >
-              <Paragraph>↑</Paragraph>
-            </TendencyPositioner>
-          ) : null}
           <Bar
             height={
               predictedIsHigher
-                ? nBarHeightMax + "rem"
-                : nBarHeightMax / 2 + "rem"
+                ? nBarHeightMax / 2 + "rem"
+                : nBarHeightMax + "rem"
             }
             width={nBarWidth + "rem"}
           >
-            <StyledGradient />
-            <Positioner textAboveBar={!userPredictsOverShoot}>
-              <Paragraph>{getNumberWithSuffix(predictedListeners)}</Paragraph>
+            <StyledGradient
+              presentationType={presentationType}
+              currentUserWins={currentUserWins}
+            />
+            <Positioner textAboveBar>
+              <Paragraph>{getNumberWithSuffix(currentListeners)}</Paragraph>
             </Positioner>
-            {!userPredictsOverShoot ? (
+          </Bar>
+          {showDifference || presentationType === "CREATE" ? (
+            <DifferenceWrapper
+              height={nBarHeightMax + "rem"}
+              width={nBarWidth + "rem"}
+            >
+              <Paragraph size="s" color="$neutral3">
+                {userPredictsOverShoot ? ">" : "<"}
+              </Paragraph>
+              <Paragraph size="s" margin="0 0 0.5rem 0" color="$neutral3">
+                {predictedIsHigher ? "+" : null}
+                {getNumberWithSuffix(predictedListeners - currentListeners)}
+              </Paragraph>
+              <Paragraph size="s" color="$neutral3">
+                {predictedIsHigher ? "+" : null}
+                {(
+                  ((predictedListeners - currentListeners) / currentListeners) *
+                  100
+                ).toFixed()}
+                %
+              </Paragraph>
+            </DifferenceWrapper>
+          ) : null}
+
+          <BarWrapper>
+            {userPredictsOverShoot ? (
               <TendencyPositioner
                 height={nBarHeightMax / 2 + "rem"}
                 width={nBarWidth + "rem"}
               >
-                <Paragraph>↓</Paragraph>
+                <Paragraph>↑</Paragraph>
               </TendencyPositioner>
             ) : null}
-          </Bar>
-        </BarWrapper>
-      </GraphSection>
-      <XAxis />
-      {endDate ? (
-        <Paragraph margin="0.5rem" size="s" color="$neutral3">
-          {formatDistanceToNow(new Date(endDate))}
-        </Paragraph>
-      ) : null}
-    </BetVisualizerWrapper>
-  );
-};
+            <Bar
+              height={
+                predictedIsHigher
+                  ? nBarHeightMax + "rem"
+                  : nBarHeightMax / 2 + "rem"
+              }
+              width={nBarWidth + "rem"}
+            >
+              <StyledGradient />
+              <Positioner textAboveBar={!userPredictsOverShoot}>
+                <Paragraph>{getNumberWithSuffix(predictedListeners)}</Paragraph>
+              </Positioner>
+              {!userPredictsOverShoot ? (
+                <TendencyPositioner
+                  height={nBarHeightMax / 2 + "rem"}
+                  width={nBarWidth + "rem"}
+                >
+                  <Paragraph>↓</Paragraph>
+                </TendencyPositioner>
+              ) : null}
+            </Bar>
+          </BarWrapper>
+        </GraphSection>
+        <XAxis />
+        {endDate ? (
+          <Paragraph margin="0.5rem" size="s" color="$neutral3">
+            {formatDistanceToNow(new Date(endDate))}
+          </Paragraph>
+        ) : null}
+      </BetVisualizerWrapper>
 
-const BetStats = ({
-  listeners,
-  predictedListeners,
-  currentListeners = 0,
-  type,
-  startDate,
-  endDate,
-  currentUserSupports,
-  quote, // TODO: this needs to be visualized
-  currentUserAmount, // TODO: this needs to be visualized
-}) => {
-  return (
-    <Wrapper>
-      <BetVisualizer
-        predictedListeners={listeners || predictedListeners}
-        currentListeners={currentListeners}
-        type={type}
-        startDate={startDate}
-        endDate={endDate}
-        currentUserSupports={currentUserSupports}
-      />
       {/* <Row>
         <Paragraph flex>Quote</Paragraph>
         <Paragraph flex>{quote ? quote : "-"}</Paragraph>
