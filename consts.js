48efb821010672f970625fedb6d57f5c8763b312
Andreas Beyer <abeyer@posteo.de>
2020-07-24 14:25:05 +0200

    prepare for deployment

--- consts.js
+++ consts.js
@@ -1,5 +1,3 @@
-export const SPOTIBET_API_DEV = "http://localhost:4000";
-export const SPOTIBET_API_PROD = "https://spotibet-api.herokuapp.com";
 export const SPOTIFY_CLIENT_ID = "5a8deb6692a34a738cf9386d357a7208";
 export const SPOTIBET_API_ENDPOINT = __DEV__
   ? "http://localhost:4000"
